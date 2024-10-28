aaaa7fbb9f24f494c87612daa058ef6881858ae8
rahul rai <xylomf2@gmail.com>
2024-10-28 22:15:29 +0000

    Data Cleaning initial step

--- Conversion_ML_Model.py
+++ Conversion_ML_Model.py
@@ -1,3 +1,11 @@
-print ("Hello")
-source = 2
-distribute = 3
+import pandas as pd
+import sklearn
+import numpy as np
+import matplotlib.pyplot as plt
+import plotly.express as px
+from sklearn.model_selection import train_test_split
+df = pd.read_csv("Conversion Data Set.csv")
+df.head()
+df.describe()
+import  seaborn as sns
+sns.scatterplot(data = df, x = df["Age"], y = df["Conversion"])
